6df808f84f7bff2e805f74df31c58a5133ad2f42
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2021-11-10 19:56:27 -0300

    terceira commit do dia

---
+++ src/Modelagem/Carro.java
@@ -0,0 +1,13 @@
+package Modelagem;
+
+import java.time.LocalDateTime;
+
+public class Carro {
+
+	private Modelo modelo;
+	private String placa;
+	private LocalDateTime entrada;
+	private LocalDateTime saida;
+	private float valor;
+
+}
---
+++ src/Principal/Principal.java
@@ -0,0 +1,36 @@
+package Principal;
+
+import java.util.ArrayList;
+
+import Modelagem.Carro;
+import Modelagem.Marca;
+import Modelagem.Modelo;
+
+public class Principal {
+	
+	// atributos static s�o atributos de classe
+	private static Carro[] vagas = new Carro[100]; // o estacionamento tem 100 vagas numeradas de 0..99
+	private static ArrayList<Marca> marcas = new ArrayList<Marca>();
+	private static ArrayList<Carro> historico = new ArrayList<Carro>();
+	
+	// eventualmente outros atributos static
+	
+	public static void main(String[] args) {
+		// outras variaveis locais
+		// menu
+		// opcao
+		// chamar metodos static que correspondam as opcoes de menu
+	}
+	
+	private static void entradaCarro() {
+		// criar o carro e cadastra-lo no vetor na posicao correta
+	}
+	
+	private static float saidaCarro() {
+		float preco = 0;
+		// logica para calcular preco do estacionamento e coloca-lo no historico
+		return preco;
+	}
+	
+	// outros m�todos static conforme especificacao do trabalho e necessidades de implementacao
+}
