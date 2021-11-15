5ecb9fce5c389990590f4cb0983ca126912d9dde
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2021-11-15 14:17:04 -0300

    Atualização de Class modelo

--- src/Modelagem/Modelo.java
+++ src/Modelagem/Modelo.java
@@ -4,5 +4,21 @@ public class Modelo {
 
 	private String nome;
 
+	public Modelo(String nome) {
+		super();
+		this.nome = nome;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
 }
