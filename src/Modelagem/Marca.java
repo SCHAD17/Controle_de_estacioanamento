c7845e7f67370cd4395e8fe201fa8cca2110eac4
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2021-11-15 14:10:48 -0300

    Atualização da class marca

--- src/Modelagem/Marca.java
+++ src/Modelagem/Marca.java
@@ -7,5 +7,48 @@ public class Marca {
 	private String nome;
 	private ArrayList<Modelo> modelos;
 
+	public Marca(String nome) {
+		this.nome = nome;
+		modelos = new ArrayList<Modelo>();
+	}
 
+	public Marca(String nome, ArrayList<Modelo> modelos) {
+		this.nome = nome;
+		this.modelos = modelos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList<Modelo> getModelos() {
+		return modelos;
+	}
+
+	public void setModelos(ArrayList<Modelo> modelos) {
+		this.modelos = modelos;
+	}
+
+	public void addModelo(String string) {
+		modelos.add(new Modelo(string));
+
+	}
+
+	public int getSizeModelo() {
+		return modelos.size();
+	}
+
+	public Modelo getNomeModelo(int j) {
+		return modelos.get(j);
+
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
 }
